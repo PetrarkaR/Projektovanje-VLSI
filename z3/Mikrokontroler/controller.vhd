68f386e58d777558ca52e1be05d080e7078545f8
Petrarka <petrarkar@proton.me>
2024-11-20 00:53:48 +0100

    Update controller.vhd

--- z3/Mikrokontroler/controller.vhd
+++ z3/Mikrokontroler/controller.vhd
@@ -2,138 +2,173 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 USE ieee.std_logic_unsigned.ALL;
 USE work.defs.ALL;
- 
-ENTITY Controller IS PORT (
-	flag_f, flag_z, reset, clk : IN STD_LOGIC;
-	IR_out : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
-	acc_bus, data_mar, data_pc, en_ram, load_acc, mar_data, oe_ram, oe_rom, pc_data, ready, wr_ir, wr_ram, wr_reg, oe_reg : OUT STD_LOGIC);
-END ENTITY Controller;
- 
-ARCHITECTURE Controller OF Controller IS
- 
-	TYPE state_type IS (R0, R1, R2, I1, I2, I3, I4, I5, I6, I7, I8, I9, I10);
-	SIGNAL present_state, next_state : state_type;
-	SIGNAL opkod : STD_LOGIC_VECTOR(4 DOWNTO 0);
-	SIGNAL cnt : STD_LOGIC_VECTOR(8 DOWNTO 0);
- 
+
+ENTITY cpu_control IS 
+    PORT (
+        -- Control inputs
+        flags_f, flags_z   : IN  STD_LOGIC;
+        reset, clk   : IN  STD_LOGIC;
+        instruction_reg    : IN  STD_LOGIC_VECTOR(7 DOWNTO 0);
+        
+        -- Control outputs
+        accumulator_out    : OUT STD_LOGIC;
+        data_to_mar       : OUT STD_LOGIC;
+        data_to_pc        : OUT STD_LOGIC;
+        ram_enable        : OUT STD_LOGIC;
+        accumulator_load  : OUT STD_LOGIC;
+        mar_to_data      : OUT STD_LOGIC;
+        ram_output_en    : OUT STD_LOGIC;
+        rom_output_en    : OUT STD_LOGIC;
+        pc_to_data       : OUT STD_LOGIC;
+        ready     : OUT STD_LOGIC;
+        ir_write_en      : OUT STD_LOGIC;
+        ram_write_en     : OUT STD_LOGIC;
+        reg_write_en     : OUT STD_LOGIC;
+        reg_output_en    : OUT STD_LOGIC
+    );
+END ENTITY cpu_control;
+
+ARCHITECTURE rtl OF cpu_control IS
+    TYPE machine_states IS (
+        RESET_0, RESET_1, RESET_2,
+        FETCH, DECODE, EXECUTE,
+        WRITE_OP, STORE_OP, JUMP_OP, NOP_OP,
+        READ_OP, LOAD_OP, ALU_OP
+    );
+    
+    -- Internal signals
+    SIGNAL current_state, next_state : state;
+    SIGNAL opcode    : STD_LOGIC_VECTOR(4 DOWNTO 0);
+    SIGNAL init_count   : STD_LOGIC_VECTOR(8 DOWNTO 0);
+
 BEGIN
- 
-	proc1 : PROCESS (clk, reset) IS
-	BEGIN
-		IF reset = '1' THEN
-			present_state <= R0;
-			cnt <= (OTHERS => '0');
-		ELSIF (clk'event AND clk = '1') THEN
-			present_state <= next_state;
-			IF (cnt = "111111111") THEN
-				cnt <= "111111111";
-			ELSE
-				cnt <= cnt + '1';
-			END IF;
-		END IF;
-	END PROCESS proc1;
- 
- opkod <= IR_out(7 DOWNTO 3);
- 
-	proc2 : PROCESS (present_state, opkod, flag_z, flag_f, cnt) IS
-	BEGIN
-		acc_bus <= '0';
-		data_mar <= '0';
-		data_pc <= '0';
-		en_ram <= '0';
-		load_acc <= '0';
-		mar_data <= '0';
-		oe_ram <= '0';
-		oe_rom <= '0';
-		pc_data <= '0';
-		ready <= '0';
-		wr_ir <= '0';
-		wr_ram <= '0';
-		wr_reg <= '0';
-		oe_reg <= '0';
- 
-		CASE (present_state) IS
-			WHEN R0 =>
-				next_state <= R1;
-			WHEN R1 =>
-				pc_data <= '1';
-				data_mar <= '1';
-				next_state <= R2;
-			WHEN R2 =>
-				oe_rom <= '1';
-				wr_ram <= '1';
-				en_ram <= '1';
-				ready <= '1';
-				IF (cnt = "111111111") THEN
-					next_state <= I1;
-				ELSE
-					next_state <= R1;
-				END IF;
-			WHEN I1 =>
-				pc_data <= '1';
-				data_mar <= '1';
-				next_state <= I2;
-			WHEN I2 =>
-				oe_ram <= '1';
-				wr_ir <= '1';
-				en_ram <= '1';
-				data_mar <= '1';
-				next_state <= I3;
-			WHEN I3 =>
-				IF opkod = write THEN
-					next_state <= I4;
-				ELSIF opkod = store THEN
-					next_state <= I5;
-				ELSIF ((opkod = jmp) OR  (opkod = jnf) OR  (opkod = jf) OR  (opkod = jnz) OR  (opkod = jz)) THEN
-					IF ((opkod = jmp) OR ((opkod = jnf) AND (flag_f = '0')) OR ((opkod = jf) AND(flag_f = '1')) OR ((opkod = jnz) AND(flag_z = '0')) OR ((opkod = jz) AND (flag_z = '1'))) THEN
-						next_state <= I6;
-					ELSE
-						next_state <= I7;
-					END IF;
-				ELSIF opkod = nop THEN
-					next_state <= I7;
-				ELSIF opkod = read THEN
-					next_state <= I8;
-				ELSIF opkod = loadl THEN
-					next_state <= I9;
-				ELSE
-					next_state <= I10;
-				END IF;
-			WHEN I4 =>
-				acc_bus <= '1';
-				wr_reg <= '1';
-				ready <= '1';
-				next_state <= I1;
-			WHEN I5 =>
-				acc_bus <= '1';
-				wr_ram <= '1';
-				en_ram <= '1';
-				ready <= '1';
-				next_state <= I1;
-			WHEN I6 =>
-				mar_data <= '1';
-				data_pc <= '1';
-				ready <= '1';
-				next_state <= I1;
-			WHEN I7 =>
-				ready <= '1';
-				next_state <= I1;
-			WHEN I8 =>
-				oe_reg <= '1';
-				load_acc <= '1';
-				ready <= '1';
-				next_state <= I1;
-			WHEN I9 =>
-				mar_data <= '1';
-				load_acc <= '1';
-				ready <= '1';
-				next_state <= I1;
-			WHEN I10 =>
-				oe_ram <= '1';
-				load_acc <= '1';
-				en_ram <= '1';
-				ready <= '1';
-				next_state <= I1;
-		END CASE;
-end process;
-	END ARCHITECTURE Controller;
+    opcode <= instruction_reg(7 DOWNTO 3);
+
+    state_and_counter: PROCESS (clk, reset) 
+    BEGIN
+        IF reset = '1' THEN
+            current_state <= RESET_0;
+            init_count <= (OTHERS => '0');
+        ELSIF rising_edge(clk) THEN
+            current_state <= next_state;
+            IF init_count = "111111111" THEN
+                init_count <= init_count;  -- Hold at max
+            ELSE
+                init_count <= init_count + 1;
+            END IF;
+        END IF;
+    END PROCESS state_and_counter;
+
+    control_logic: PROCESS (current_state, opcode, flags_z, flags_f, init_count) 
+    BEGIN
+        accumulator_out <= '0'; data_to_mar <= '0'; data_to_pc <= '0';
+        ram_enable <= '0'; accumulator_load <= '0'; mar_to_data <= '0';
+        ram_output_en <= '0'; rom_output_en <= '0'; pc_to_data <= '0';
+        ready <= '0'; ir_write_en <= '0'; ram_write_en <= '0';
+        reg_write_en <= '0'; reg_output_en <= '0';
+
+        CASE current_state IS
+            WHEN RESET_0 =>
+                next_state <= RESET_1;
+
+            WHEN RESET_1 =>
+                pc_to_data <= '1';
+                data_to_mar <= '1';
+                next_state <= RESET_2;
+
+            WHEN RESET_2 =>
+                rom_output_en <= '1';
+                ram_write_en <= '1';
+                ram_enable <= '1';
+                ready <= '1';
+                IF init_count = "111111111" THEN
+                    next_state <= FETCH;
+                ELSE
+                    next_state <= RESET_1;
+                END IF;
+
+            WHEN FETCH =>
+                pc_to_data <= '1';
+                data_to_mar <= '1';
+                next_state <= DECODE;
+
+            WHEN DECODE =>
+                ram_output_en <= '1';
+                ir_write_en <= '1';
+                ram_enable <= '1';
+                data_to_mar <= '1';
+                next_state <= EXECUTE;
+
+            WHEN EXECUTE =>
+                IF opcode = write THEN
+                    next_state <= WRITE_OP;
+                ELSIF opcode = store THEN
+                    next_state <= STORE_OP;
+                ELSIF (opcode = jmp) OR (opcode = jnf) OR 
+                      (opcode = jf) OR (opcode = jnz) OR 
+                      (opcode = jz) THEN
+                    IF (opcode = jmp) OR 
+                       (opcode = jnf AND flags_f = '0') OR 
+                       (opcode = jf AND flags_f = '1') OR 
+                       (opcode = jnz AND flags_z = '0') OR 
+                       (opcode = jz AND flags_z = '1') THEN
+                        next_state <= JUMP_OP;
+                    ELSE
+                        next_state <= NOP_OP;
+                    END IF;
+                ELSIF opcode = nop THEN
+                    next_state <= NOP_OP;
+                ELSIF opcode = read THEN
+                    next_state <= READ_OP;
+                ELSIF opcode = loadl THEN
+                    next_state <= LOAD_OP;
+                ELSE
+                    next_state <= ALU_OP;
+                END IF;
+
+            WHEN WRITE_OP =>
+                accumulator_out <= '1';
+                reg_write_en <= '1';
+                ready <= '1';
+                next_state <= FETCH;
+
+            WHEN STORE_OP =>
+                accumulator_out <= '1';
+                ram_write_en <= '1';
+                ram_enable <= '1';
+                ready <= '1';
+                next_state <= FETCH;
+
+            WHEN JUMP_OP =>
+                mar_to_data <= '1';
+                data_to_pc <= '1';
+                ready <= '1';
+                next_state <= FETCH;
+
+            WHEN NOP_OP =>
+                ready <= '1';
+                next_state <= FETCH;
+
+            WHEN READ_OP =>
+                reg_output_en <= '1';
+                accumulator_load <= '1';
+                ready <= '1';
+                next_state <= FETCH;
+
+            WHEN LOAD_OP =>
+                mar_to_data <= '1';
+                accumulator_load <= '1';
+                ready <= '1';
+                next_state <= FETCH;
+
+            WHEN ALU_OP =>
+                ram_output_en <= '1';
+                accumulator_load <= '1';
+                ram_enable <= '1';
+                ready <= '1';
+                next_state <= FETCH;
+
+        END CASE;
+    END PROCESS control_logic;
+
+END ARCHITECTURE rtl;
